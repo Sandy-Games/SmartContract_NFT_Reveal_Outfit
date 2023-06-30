b6421912f5e5f8ec300d6e154b3c3937f7e3cf76
gurudev0807 <stareast0807@gmail.com>
2023-06-30 04:34:24 +0100

    feat: Body Minting Smart Contract for randomization

--- SANDY-MAIN/Output/character.asm
+++ SANDY-MAIN/Output/character.asm
@@ -1,7 +1,7 @@
-// Line 78:        }
-// Line 79:    }
-// Line 80:
-// Line 81:    nft character<body_rom, body_ram> {
+// Line 106:        }
+// Line 107:    }
+// Line 108:
+// Line 109:    nft character<body_rom, body_ram> {
 
 // ********* getName Property ***********
 @entry_getName: // 0
@@ -20,8 +20,8 @@ ALIAS r3 $_ROM // 52
 LOAD r0 "ROM" // 52
 GET r2 $_ROM r0 // 59
 UNPACK $_ROM $_ROM // 63
-// Line 82:        property name: string{
-// Line 83:            return _ROM.name;
+// Line 110:        property name: string{
+// Line 111:            return _ROM.name;
 	COPY $_ROM r1 // 66
 	LOAD r2 "name" // 69
 	GET r1 r1 r2 // 77
@@ -29,7 +29,7 @@ UNPACK $_ROM $_ROM // 63
 	JMP @exit_getName // 83
 @exit_getName: // 86
 RET // 87
-// Line 84:        }
+// Line 112:        }
 
 // ********* getDescription Property ***********
 @entry_getDescription: // 88
@@ -48,9 +48,9 @@ ALIAS r3 $_ROM // 140
 LOAD r0 "ROM" // 140
 GET r2 $_ROM r0 // 147
 UNPACK $_ROM $_ROM // 151
-// Line 85:
-// Line 86:        property description: string {
-// Line 87:            return _ROM.description;
+// Line 113:
+// Line 114:        property description: string {
+// Line 115:            return _ROM.description;
 	COPY $_ROM r1 // 154
 	LOAD r2 "description" // 157
 	GET r1 r1 r2 // 172
@@ -58,7 +58,7 @@ UNPACK $_ROM $_ROM // 151
 	JMP @exit_getDescription // 178
 @exit_getDescription: // 181
 RET // 182
-// Line 88:        }
+// Line 116:        }
 
 // ********* getInfoURL Property ***********
 @entry_getInfoURL: // 183
@@ -77,9 +77,9 @@ ALIAS r3 $_ROM // 235
 LOAD r0 "ROM" // 235
 GET r2 $_ROM r0 // 242
 UNPACK $_ROM $_ROM // 246
-// Line 89:
-// Line 90:        property infoURL: string {
-// Line 91:            return _ROM.infoURL;
+// Line 117:
+// Line 118:        property infoURL: string {
+// Line 119:            return _ROM.infoURL;
 	COPY $_ROM r1 // 249
 	LOAD r2 "infoURL" // 252
 	GET r1 r1 r2 // 263
@@ -87,7 +87,7 @@ UNPACK $_ROM $_ROM // 246
 	JMP @exit_getInfoURL // 269
 @exit_getInfoURL: // 272
 RET // 273
-// Line 92:        }
+// Line 120:        }
 
 // ********* getImageURL Property ***********
 @entry_getImageURL: // 274
@@ -106,8 +106,8 @@ ALIAS r3 $_ROM // 326
 LOAD r0 "ROM" // 326
 GET r2 $_ROM r0 // 333
 UNPACK $_ROM $_ROM // 337
-// Line 93:        property imageURL: string {
-// Line 94:           return  _ROM.imageURL;
+// Line 121:        property imageURL: string {
+// Line 122:           return  _ROM.imageURL;
 	COPY $_ROM r1 // 340
 	LOAD r2 "imageURL" // 343
 	GET r1 r1 r2 // 355
@@ -115,7 +115,7 @@ UNPACK $_ROM $_ROM // 337
 	JMP @exit_getImageURL // 361
 @exit_getImageURL: // 364
 RET // 365
-// Line 95:        }
+// Line 123:        }
 
 // ********* getCreated Property ***********
 @entry_getCreated: // 366
@@ -134,9 +134,9 @@ ALIAS r3 $_ROM // 418
 LOAD r0 "ROM" // 418
 GET r2 $_ROM r0 // 425
 UNPACK $_ROM $_ROM // 429
-// Line 96:
-// Line 97:        property created: timestamp { 
-// Line 98:            return _ROM.created;
+// Line 124:
+// Line 125:        property created: timestamp { 
+// Line 126:            return _ROM.created;
 	COPY $_ROM r1 // 432
 	LOAD r2 "created" // 435
 	GET r1 r1 r2 // 446
@@ -144,7 +144,7 @@ UNPACK $_ROM $_ROM // 429
 	JMP @exit_getCreated // 452
 @exit_getCreated: // 455
 RET // 456
-// Line 99:        }
+// Line 127:        }
 
 // ********* getSandType Property ***********
 @entry_getSandType: // 457
@@ -163,9 +163,9 @@ ALIAS r3 $_ROM // 509
 LOAD r0 "ROM" // 509
 GET r2 $_ROM r0 // 516
 UNPACK $_ROM $_ROM // 520
-// Line 100:
-// Line 101:        property sandType: string {
-// Line 102:            return _ROM.sandType;
+// Line 128:
+// Line 129:        property sandType: string {
+// Line 130:            return _ROM.sandType;
 	COPY $_ROM r1 // 523
 	LOAD r2 "sandType" // 526
 	GET r1 r1 r2 // 538
@@ -173,7 +173,7 @@ UNPACK $_ROM $_ROM // 520
 	JMP @exit_getSandType // 544
 @exit_getSandType: // 547
 RET // 548
-// Line 103:        }
+// Line 131:        }
 
 // ********* getSkinType Property ***********
 @entry_getSkinType: // 549
@@ -192,9 +192,9 @@ ALIAS r3 $_ROM // 601
 LOAD r0 "ROM" // 601
 GET r2 $_ROM r0 // 608
 UNPACK $_ROM $_ROM // 612
-// Line 104:
-// Line 105:        property skinType: string {
-// Line 106:            return _ROM.skinType;
+// Line 132:
+// Line 133:        property skinType: string {
+// Line 134:            return _ROM.skinType;
 	COPY $_ROM r1 // 615
 	LOAD r2 "skinType" // 618
 	GET r1 r1 r2 // 630
@@ -202,7 +202,7 @@ UNPACK $_ROM $_ROM // 612
 	JMP @exit_getSkinType // 636
 @exit_getSkinType: // 639
 RET // 640
-// Line 107:        }
+// Line 135:        }
 
 // ********* getBodyType Property ***********
 @entry_getBodyType: // 641
@@ -221,9 +221,9 @@ ALIAS r3 $_ROM // 693
 LOAD r0 "ROM" // 693
 GET r2 $_ROM r0 // 700
 UNPACK $_ROM $_ROM // 704
-// Line 108:
-// Line 109:        property bodyType: number {
-// Line 110:            return _ROM.bodyType;
+// Line 136:
+// Line 137:        property bodyType: number {
+// Line 138:            return _ROM.bodyType;
 	COPY $_ROM r1 // 707
 	LOAD r2 "bodyType" // 710
 	GET r1 r1 r2 // 722
@@ -231,3 +231,232 @@ UNPACK $_ROM $_ROM // 704
 	JMP @exit_getBodyType // 728
 @exit_getBodyType: // 731
 RET // 732
+// Line 139:        }
+
+// ********* getCategoryName Property ***********
+@entry_getCategoryName: // 733
+POP r1 // get nft tokenID from stack // 734
+// reading nft data
+LOAD r0 "ROM" // 736
+PUSH r0 // fields // 743
+PUSH r1 // tokenID // 745
+LOAD r0 "STST" // 747
+PUSH r0 // symbol // 755
+LOAD r0 "Runtime.ReadToken" // 757
+EXTCALL r0 // 778
+POP r2 // 780
+UNPACK r2 r2 // 782
+ALIAS r3 $_ROM // 785
+LOAD r0 "ROM" // 785
+GET r2 $_ROM r0 // 792
+UNPACK $_ROM $_ROM // 796
+// Line 140:
+// Line 141:        property categoryName: string {
+// Line 142:            local index: number = _ROM.bodyType;
+	ALIAS r1 $index // 799
+	COPY $_ROM r2 // 799
+	LOAD r4 "bodyType" // 802
+	GET r2 r2 r4 // 814
+	COPY r2 $index // 818
+// Line 143:
+// Line 144:            if (_ROM.skinType == "Energy") {
+	COPY $_ROM r2 // 821
+	LOAD r4 "skinType" // 824
+	GET r2 r2 r4 // 836
+	ALIAS r4 $literalexpression423 // 840
+	LOAD $literalexpression423 "Energy" // 840
+	EQUAL r2 $literalexpression423 r5 // 850
+		JMPNOT r5 @else_ifstatement421 // 854
+// Line 145:                local energyArray: array<string> = {"Glass", "Ice", "Lightning", "Reborn", "Sand Storm", "Smoke", "Soul Glass", "Volcano Ash", "Volcano Rock", "White Sand Storm"};
+		ALIAS r2 $energyArray // 858
+		ALIAS r4 $arrayexpression426 // 858
+		CLEAR $arrayexpression426 // 858
+		ALIAS r6 $_array_init_idx // 860
+		ALIAS r7 $literalexpression427 // 860
+		LOAD $literalexpression427 "Glass" // 860
+		LOAD $_array_init_idx 0 // 869
+		PUT $literalexpression427 $arrayexpression426 $_array_init_idx // 874
+		ALIAS r7 $literalexpression428 // 878
+		LOAD $literalexpression428 "Ice" // 878
+		LOAD $_array_init_idx 1 // 885
+		PUT $literalexpression428 $arrayexpression426 $_array_init_idx // 890
+		ALIAS r7 $literalexpression429 // 894
+		LOAD $literalexpression429 "Lightning" // 894
+		LOAD $_array_init_idx 2 // 907
+		PUT $literalexpression429 $arrayexpression426 $_array_init_idx // 912
+		ALIAS r7 $literalexpression430 // 916
+		LOAD $literalexpression430 "Reborn" // 916
+		LOAD $_array_init_idx 3 // 926
+		PUT $literalexpression430 $arrayexpression426 $_array_init_idx // 931
+		ALIAS r7 $literalexpression431 // 935
+		LOAD $literalexpression431 "Sand Storm" // 935
+		LOAD $_array_init_idx 4 // 949
+		PUT $literalexpression431 $arrayexpression426 $_array_init_idx // 954
+		ALIAS r7 $literalexpression432 // 958
+		LOAD $literalexpression432 "Smoke" // 958
+		LOAD $_array_init_idx 5 // 967
+		PUT $literalexpression432 $arrayexpression426 $_array_init_idx // 972
+		ALIAS r7 $literalexpression433 // 976
+		LOAD $literalexpression433 "Soul Glass" // 976
+		LOAD $_array_init_idx 6 // 990
+		PUT $literalexpression433 $arrayexpression426 $_array_init_idx // 995
+		ALIAS r7 $literalexpression434 // 999
+		LOAD $literalexpression434 "Volcano Ash" // 999
+		LOAD $_array_init_idx 7 // 1014
+		PUT $literalexpression434 $arrayexpression426 $_array_init_idx // 1019
+		ALIAS r7 $literalexpression435 // 1023
+		LOAD $literalexpression435 "Volcano Rock" // 1023
+		LOAD $_array_init_idx 8 // 1039
+		PUT $literalexpression435 $arrayexpression426 $_array_init_idx // 1044
+		ALIAS r7 $literalexpression436 // 1048
+		LOAD $literalexpression436 "White Sand Storm" // 1048
+		LOAD $_array_init_idx 9 // 1068
+		PUT $literalexpression436 $arrayexpression426 $_array_init_idx // 1073
+		COPY $arrayexpression426 $energyArray // 1077
+// Line 146:                
+// Line 147:                return energyArray[index];
+		COPY $index r6 // 1080
+		GET $energyArray r4 r6 // 1083
+		PUSH r4 // 1087
+		JMP @exit_getCategoryName // 1089
+		JMP @then_ifstatement421 // 1092
+		@else_ifstatement421: NOP // 1096
+// Line 148:            } else if (_ROM.skinType == "Organic") {
+		COPY $_ROM r4 // 1096
+		LOAD r6 "skinType" // 1099
+		GET r4 r4 r6 // 1111
+		ALIAS r6 $literalexpression445 // 1115
+		LOAD $literalexpression445 "Organic" // 1115
+		EQUAL r4 $literalexpression445 r7 // 1126
+			JMPNOT r7 @else_ifstatement443 // 1130
+// Line 149:                local organicArray: array<string> = {"Burlap Blue","Burlap Brown","Burlap DarkGrey","Burlap Green","Burlap Grey&Brown","Burlap Grey&Red","Burlap Red","Burlap White","Burlap White&Blue","Burlap White&Brown","Burlap White&Red"};
+			ALIAS r4 $organicArray // 1134
+			ALIAS r6 $arrayexpression448 // 1134
+			CLEAR $arrayexpression448 // 1134
+			ALIAS r8 $_array_init_idx // 1136
+			ALIAS r9 $literalexpression449 // 1136
+			LOAD $literalexpression449 "Burlap Blue" // 1136
+			LOAD $_array_init_idx 0 // 1151
+			PUT $literalexpression449 $arrayexpression448 $_array_init_idx // 1156
+			ALIAS r9 $literalexpression450 // 1160
+			LOAD $literalexpression450 "Burlap Brown" // 1160
+			LOAD $_array_init_idx 1 // 1176
+			PUT $literalexpression450 $arrayexpression448 $_array_init_idx // 1181
+			ALIAS r9 $literalexpression451 // 1185
+			LOAD $literalexpression451 "Burlap DarkGrey" // 1185
+			LOAD $_array_init_idx 2 // 1204
+			PUT $literalexpression451 $arrayexpression448 $_array_init_idx // 1209
+			ALIAS r9 $literalexpression452 // 1213
+			LOAD $literalexpression452 "Burlap Green" // 1213
+			LOAD $_array_init_idx 3 // 1229
+			PUT $literalexpression452 $arrayexpression448 $_array_init_idx // 1234
+			ALIAS r9 $literalexpression453 // 1238
+			LOAD $literalexpression453 "Burlap Grey&Brown" // 1238
+			LOAD $_array_init_idx 4 // 1259
+			PUT $literalexpression453 $arrayexpression448 $_array_init_idx // 1264
+			ALIAS r9 $literalexpression454 // 1268
+			LOAD $literalexpression454 "Burlap Grey&Red" // 1268
+			LOAD $_array_init_idx 5 // 1287
+			PUT $literalexpression454 $arrayexpression448 $_array_init_idx // 1292
+			ALIAS r9 $literalexpression455 // 1296
+			LOAD $literalexpression455 "Burlap Red" // 1296
+			LOAD $_array_init_idx 6 // 1310
+			PUT $literalexpression455 $arrayexpression448 $_array_init_idx // 1315
+			ALIAS r9 $literalexpression456 // 1319
+			LOAD $literalexpression456 "Burlap White" // 1319
+			LOAD $_array_init_idx 7 // 1335
+			PUT $literalexpression456 $arrayexpression448 $_array_init_idx // 1340
+			ALIAS r9 $literalexpression457 // 1344
+			LOAD $literalexpression457 "Burlap White&Blue" // 1344
+			LOAD $_array_init_idx 8 // 1365
+			PUT $literalexpression457 $arrayexpression448 $_array_init_idx // 1370
+			ALIAS r9 $literalexpression458 // 1374
+			LOAD $literalexpression458 "Burlap White&Brown" // 1374
+			LOAD $_array_init_idx 9 // 1396
+			PUT $literalexpression458 $arrayexpression448 $_array_init_idx // 1401
+			ALIAS r9 $literalexpression459 // 1405
+			LOAD $literalexpression459 "Burlap White&Red" // 1405
+			LOAD $_array_init_idx 10 // 1425
+			PUT $literalexpression459 $arrayexpression448 $_array_init_idx // 1430
+			COPY $arrayexpression448 $organicArray // 1434
+// Line 150:                
+// Line 151:                return organicArray[index];
+			COPY $index r8 // 1437
+			GET $organicArray r6 r8 // 1440
+			PUSH r6 // 1444
+			JMP @exit_getCategoryName // 1446
+			JMP @then_ifstatement443 // 1449
+			@else_ifstatement443: NOP // 1453
+// Line 152:            } else if (_ROM.skinType == "Precious") {
+			COPY $_ROM r6 // 1453
+			LOAD r8 "skinType" // 1456
+			GET r6 r6 r8 // 1468
+			ALIAS r8 $literalexpression468 // 1472
+			LOAD $literalexpression468 "Precious" // 1472
+			EQUAL r6 $literalexpression468 r9 // 1484
+				JMPNOT r9 @then_ifstatement466 // 1488
+// Line 153:                local preciousArray: array<string> = {"Bronze", "Circuit", "Diamond", "Gold", "Iron", "Lizard", "Pink Diamond", "Pink Stone", "Rose Diamond", "Rose Stone", "Silver", "White Fur"};
+				ALIAS r6 $preciousArray // 1492
+				ALIAS r8 $arrayexpression471 // 1492
+				CLEAR $arrayexpression471 // 1492
+				ALIAS r10 $_array_init_idx // 1494
+				ALIAS r11 $literalexpression472 // 1494
+				LOAD $literalexpression472 "Bronze" // 1494
+				LOAD $_array_init_idx 0 // 1504
+				PUT $literalexpression472 $arrayexpression471 $_array_init_idx // 1509
+				ALIAS r11 $literalexpression473 // 1513
+				LOAD $literalexpression473 "Circuit" // 1513
+				LOAD $_array_init_idx 1 // 1524
+				PUT $literalexpression473 $arrayexpression471 $_array_init_idx // 1529
+				ALIAS r11 $literalexpression474 // 1533
+				LOAD $literalexpression474 "Diamond" // 1533
+				LOAD $_array_init_idx 2 // 1544
+				PUT $literalexpression474 $arrayexpression471 $_array_init_idx // 1549
+				ALIAS r11 $literalexpression475 // 1553
+				LOAD $literalexpression475 "Gold" // 1553
+				LOAD $_array_init_idx 3 // 1561
+				PUT $literalexpression475 $arrayexpression471 $_array_init_idx // 1566
+				ALIAS r11 $literalexpression476 // 1570
+				LOAD $literalexpression476 "Iron" // 1570
+				LOAD $_array_init_idx 4 // 1578
+				PUT $literalexpression476 $arrayexpression471 $_array_init_idx // 1583
+				ALIAS r11 $literalexpression477 // 1587
+				LOAD $literalexpression477 "Lizard" // 1587
+				LOAD $_array_init_idx 5 // 1597
+				PUT $literalexpression477 $arrayexpression471 $_array_init_idx // 1602
+				ALIAS r11 $literalexpression478 // 1606
+				LOAD $literalexpression478 "Pink Diamond" // 1606
+				LOAD $_array_init_idx 6 // 1622
+				PUT $literalexpression478 $arrayexpression471 $_array_init_idx // 1627
+				ALIAS r11 $literalexpression479 // 1631
+				LOAD $literalexpression479 "Pink Stone" // 1631
+				LOAD $_array_init_idx 7 // 1645
+				PUT $literalexpression479 $arrayexpression471 $_array_init_idx // 1650
+				ALIAS r11 $literalexpression480 // 1654
+				LOAD $literalexpression480 "Rose Diamond" // 1654
+				LOAD $_array_init_idx 8 // 1670
+				PUT $literalexpression480 $arrayexpression471 $_array_init_idx // 1675
+				ALIAS r11 $literalexpression481 // 1679
+				LOAD $literalexpression481 "Rose Stone" // 1679
+				LOAD $_array_init_idx 9 // 1693
+				PUT $literalexpression481 $arrayexpression471 $_array_init_idx // 1698
+				ALIAS r11 $literalexpression482 // 1702
+				LOAD $literalexpression482 "Silver" // 1702
+				LOAD $_array_init_idx 10 // 1712
+				PUT $literalexpression482 $arrayexpression471 $_array_init_idx // 1717
+				ALIAS r11 $literalexpression483 // 1721
+				LOAD $literalexpression483 "White Fur" // 1721
+				LOAD $_array_init_idx 11 // 1734
+				PUT $literalexpression483 $arrayexpression471 $_array_init_idx // 1739
+				COPY $arrayexpression471 $preciousArray // 1743
+// Line 154:                
+// Line 155:                return preciousArray[index];
+				COPY $index r10 // 1746
+				GET $preciousArray r8 r10 // 1749
+				PUSH r8 // 1753
+				JMP @exit_getCategoryName // 1755
+				@then_ifstatement466: NOP // 1759
+			@then_ifstatement443: NOP // 1760
+		@then_ifstatement421: NOP // 1761
+@exit_getCategoryName: // 1761
+RET // 1762
--- SANDY-MAIN/Output/limb.asm
+++ SANDY-MAIN/Output/limb.asm
@@ -25,42 +25,70 @@
 // Line 25:    bodyType: number;           // Could be in range of 1 ~ 
 // Line 26:}
 // Line 27:
-// Line 28://Will change once complete
-// Line 29:token STST {
-// Line 30:    import Array;
-// Line 31:    import NFT;
-// Line 32:    import Runtime;
-// Line 33:    import Time;
-// Line 34:    import Token;
-// Line 35:    import Map;
-// Line 36:    import Random;
-// Line 37:
-// Line 38:    global owner: address;
-// Line 39:    global owns_sandy: storage_map<address,bool>; 
-// Line 40:    global _infusionAmt: number;
-// Line 41:    /*
-// Line 42:     - Can use the seriesID as the number and can use randomization that way. Will have to think more about this
-// Line 43:    */
-// Line 44:    global _limbs: storage_map<string,number>; //wanted this to be an array so I can get index by modulus. Will test out.
-// Line 45:    global _categories: storage_map<string,number>;
-// Line 46:    global _infusionTkn: string;
-// Line 47:
-// Line 48:
-// Line 49:	const DOLL_SUPPLY: number = 500;
-// Line 50:    const SANDY_SERIESID: number = 1;
-// Line 51:
-// Line 52:    property name: string = "Test Sandy";    //Will change to SANDY once thoroughly tested
-// Line 53:    property symbol: string = "STST";  // Will change once properly tested
-// Line 54:    property isFungible: bool = false;
-// Line 55:    property isFinite: bool = true;
-// Line 56:    property isBurnable: bool = true;
-// Line 57:    property maxSupply: number = DOLL_SUPPLY;
-// Line 58:    property isTransferable: bool = true;
-// Line 59:    property isCapped: bool = false;
-// Line 60:    property decimals:number = 8;
-// Line 61:    property owner:address = owner;
-// Line 62:    
-// Line 63:    nft limb<limb_rom,limb_ram> {
+// Line 28:enum SkinCount {
+// Line 29:    Organic = 175,
+// Line 30:    Precious = 175,
+// Line 31:    Energy = 150
+// Line 32:}
+// Line 33:
+// Line 34://Will change once complete
+// Line 35:token STST {
+// Line 36:    import Array;
+// Line 37:    import NFT;
+// Line 38:    import Runtime;
+// Line 39:    import Time;
+// Line 40:    import Token;
+// Line 41:    import Map;
+// Line 42:    import Random;
+// Line 43:    import Stake;
+// Line 44:
+// Line 45:    global owner: address;
+// Line 46:
+// Line 47:    global owns_sandy: storage_map<address,bool>; 
+// Line 48:    global balances: storage_map <address, number>;
+// Line 49:    global _infusionAmt: number;
+// Line 50:
+// Line 51:    global mintBodyInited: bool;
+// Line 52:    global bodyBaseURI: string;
+// Line 53:
+// Line 54:    global _limbs: storage_map<string,number>; //wanted this to be an array so I can get index by modulus. Will test out.
+// Line 55:    global _categories: storage_map<string,number>;
+// Line 56:    global _infusionTkn: string;
+// Line 57:
+// Line 58:	const DOLL_SUPPLY: number = 500;
+// Line 59:
+// Line 60:    /**
+// Line 61:    * Constant variables for sandy NFT series IDs.
+// Line 62:    */
+// Line 63:    const SERIES_BODY: number = 1;
+// Line 64:    const SERIES_EYE: number = 2;
+// Line 65:    const SERIES_MOUTH: number = 3;
+// Line 66:    const SERIES_BACKGROUND: number = 4;
+// Line 67:
+// Line 68:    /**
+// Line 69:    * Constant variables for sandy max supply 
+// Line 70:    */
+// Line 71:
+// Line 72:    global organicCount: number;
+// Line 73:    global preciousCount: number;
+// Line 74:    global energyCount: number;
+// Line 75:
+// Line 76:    const ORGANIC_CATEGORIES: number = 12;
+// Line 77:    const PRECIOUS_CATEGORIES: number = 12;
+// Line 78:    const ENERGY_CATEGORIES: number = 10;
+// Line 79:
+// Line 80:    property name: string = "Test Sandy";    //Will change to SANDY once thoroughly tested
+// Line 81:    property symbol: string = "STST";  // Will change once properly tested
+// Line 82:    property isFungible: bool = false;
+// Line 83:    property isFinite: bool = true;
+// Line 84:    property isBurnable: bool = true;
+// Line 85:    property maxSupply: number = DOLL_SUPPLY;
+// Line 86:    property isTransferable: bool = true;
+// Line 87:    property isCapped: bool = false;
+// Line 88:    property decimals:number = 8;
+// Line 89:    property owner:address = owner;
+// Line 90:    
+// Line 91:    nft limb<limb_rom,limb_ram> {
 
 // ********* getName Property ***********
 @entry_getName: // 0
@@ -79,8 +107,8 @@ ALIAS r3 $_ROM // 52
 LOAD r0 "ROM" // 52
 GET r2 $_ROM r0 // 59
 UNPACK $_ROM $_ROM // 63
-// Line 64:        property name: string{
-// Line 65:            return _ROM.name;
+// Line 92:        property name: string{
+// Line 93:            return _ROM.name;
 	COPY $_ROM r1 // 66
 	LOAD r2 "name" // 69
 	GET r1 r1 r2 // 77
@@ -88,7 +116,7 @@ UNPACK $_ROM $_ROM // 63
 	JMP @exit_getName // 83
 @exit_getName: // 86
 RET // 87
-// Line 66:        }
+// Line 94:        }
 
 // ********* getDescription Property ***********
 @entry_getDescription: // 88
@@ -107,9 +135,9 @@ ALIAS r3 $_ROM // 140
 LOAD r0 "ROM" // 140
 GET r2 $_ROM r0 // 147
 UNPACK $_ROM $_ROM // 151
-// Line 67:
-// Line 68:        property description: string{
-// Line 69:            return _ROM.description;
+// Line 95:
+// Line 96:        property description: string{
+// Line 97:            return _ROM.description;
 	COPY $_ROM r1 // 154
 	LOAD r2 "description" // 157
 	GET r1 r1 r2 // 172
@@ -117,7 +145,7 @@ UNPACK $_ROM $_ROM // 151
 	JMP @exit_getDescription // 178
 @exit_getDescription: // 181
 RET // 182
-// Line 70:        }
+// Line 98:        }
 
 // ********* getInfoURL Property ***********
 @entry_getInfoURL: // 183
@@ -136,9 +164,9 @@ ALIAS r3 $_ROM // 235
 LOAD r0 "ROM" // 235
 GET r2 $_ROM r0 // 242
 UNPACK $_ROM $_ROM // 246
-// Line 71:
-// Line 72:        property infoURL: string{
-// Line 73:            return _ROM.infoURL;
+// Line 99:
+// Line 100:        property infoURL: string{
+// Line 101:            return _ROM.infoURL;
 	COPY $_ROM r1 // 249
 	LOAD r2 "infoURL" // 252
 	GET r1 r1 r2 // 263
@@ -146,7 +174,7 @@ UNPACK $_ROM $_ROM // 246
 	JMP @exit_getInfoURL // 269
 @exit_getInfoURL: // 272
 RET // 273
-// Line 74:        }
+// Line 102:        }
 
 // ********* getImageURL Property ***********
 @entry_getImageURL: // 274
@@ -165,9 +193,9 @@ ALIAS r3 $_ROM // 326
 LOAD r0 "ROM" // 326
 GET r2 $_ROM r0 // 333
 UNPACK $_ROM $_ROM // 337
-// Line 75:
-// Line 76:        property imageURL: string{
-// Line 77:            return _ROM.imageURL;
+// Line 103:
+// Line 104:        property imageURL: string{
+// Line 105:            return _ROM.imageURL;
 	COPY $_ROM r1 // 340
 	LOAD r2 "imageURL" // 343
 	GET r1 r1 r2 // 355
